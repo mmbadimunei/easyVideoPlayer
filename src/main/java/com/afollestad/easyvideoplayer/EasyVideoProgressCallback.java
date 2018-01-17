44c4283db48cf4ebe2277407e3d26740f8f88eab
Eisenhower <eisenhower@Eisenhowers-MacBook-Pro.local>
2018-01-17 13:42:33 +0200

    EasyVideoPlayer

---
+++ src/main/java/com/afollestad/easyvideoplayer/EasyVideoProgressCallback.java
@@ -0,0 +1,7 @@
+package com.afollestad.easyvideoplayer;
+
+/** @author Aidan Follestad (afollestad) */
+public interface EasyVideoProgressCallback {
+
+  void onVideoProgressUpdate(int position, int duration);
+}
